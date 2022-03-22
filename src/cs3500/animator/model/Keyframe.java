cbc282bcf8351cb45384138482cff80731a1f34d
Joel Willick <willickjb@gmail.com>
2022-03-21 21:47:02 -0400

    KeyFrame Interface

--- src/cs3500/animator/model/Keyframe.java
+++ src/cs3500/animator/model/Keyframe.java
@@ -1,4 +1,30 @@
 package cs3500.animator.model;
 
+/**
+ * Represents a shape at a specific time in the animation. Used to interpolate where the shape is
+ * at any given time.
+ */
 public interface Keyframe {
+
+  /**
+   * Returns the shape of this keyframe.
+   * @return the shape of this keyframe.
+   */
+  Shape getShape();
+
+  /**
+   * Returns the time this keyframe exists at.
+   * @return the time of this keyframe.
+   */
+  int getTime();
+
+  /**
+   * Interpolates the shape that exists at the time in between.
+   * @param otherKeyframe the other KeyFrame to interpolate the shape from, the name of the shape
+   *                      from the other keyframe must be the same.
+   * @param interpolateTime the time that the interpolated shape exists at.
+   * @return The shape that exists between this keyframe and otherKeyFrame at the time
+   * interpolateTime
+   */
+  Shape interpolate(Keyframe otherKeyframe, int interpolateTime);
 }
