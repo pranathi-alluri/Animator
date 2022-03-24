72ea582e91aa9f10fc38518ffd169b34f5a171b5
Joel Willick <willickjb@gmail.com>
2022-03-23 21:49:36 -0400

    tests of SimpleKeyframe.Interpolate()

--- test/SimpleKeyframeTest.java
+++ test/SimpleKeyframeTest.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 import cs3500.animator.model.Keyframe;
 import cs3500.animator.model.Shape;
-import cs3500.animator.model.ShapeType;
 import cs3500.animator.model.SimpleKeyframe;
+import cs3500.animator.model.SimpleOval;
 import cs3500.animator.model.SimpleRectangle;
 
 import static org.junit.Assert.assertEquals;
@@ -77,5 +77,54 @@ public class SimpleKeyframeTest {
     Keyframe keyframe1 = new SimpleKeyframe(this.shape, this.time);
     Keyframe keyframe2 = new SimpleKeyframe(this.shape, this.time);
     assertEquals(keyframe1.hashCode(), keyframe2.hashCode());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullKeyframeInterpolate() {
+    Keyframe kf = new SimpleKeyframe(this.shape, this.time);
+    kf.interpolate(null, 10);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIllegalTimeInterpolate() {
+    Keyframe kf = new SimpleKeyframe(this.shape, this.time);
+    Shape shape2 = new SimpleRectangle("red square", 30, 30, new Color(255, 0, 0), 40, 10);
+    Keyframe kf2 = new SimpleKeyframe(shape2, 40);
+    kf.interpolate(kf2, 5);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIllegalNameInterpolate() {
+    Keyframe kf = new SimpleKeyframe(this.shape, this.time);
+    Shape shape2 = new SimpleRectangle("red rectangle", 30, 30, new Color(255, 0, 0), 40, 10);
+    Keyframe kf2 = new SimpleKeyframe(shape2, 40);
+    kf.interpolate(kf2, 30);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIllegalShapeInterpolate() {
+    Keyframe kf = new SimpleKeyframe(this.shape, this.time);
+    Shape shape2 = new SimpleOval("red square", 30, 30, new Color(255, 0, 0), 40, 10);
+    Keyframe kf2 = new SimpleKeyframe(shape2, 40);
+    kf.interpolate(kf2, 30);
+  }
+
+  @Test
+  public void testInterpolate() {
+    Keyframe kf = new SimpleKeyframe(this.shape, this.time);
+    Shape shape2 = new SimpleRectangle("red square", 30, 30, new Color(255, 0, 0), 40, 10);
+    Keyframe kf2 = new SimpleKeyframe(shape2, 40);
+    Shape interpolatedShape = new SimpleRectangle("red square", 25, 25, new Color(255, 0, 0), 30, 15);
+    assertEquals(kf.interpolate(kf2, 30), interpolatedShape);
+  }
+
+  @Test
+  public void testInterpolate2() {
+    Shape shape1 = new SimpleOval("Oval", 10, 10, new Color(0, 200, 100), 10, 10);
+    Shape shape2 = new SimpleOval("Oval", 50, 90, new Color(100, 100, 200), 10, 30);
+    Shape interpolatedShape = new SimpleOval("Oval", 20, 30, new Color(25, 175, 125), 10, 15);
+    Keyframe kf1 = new SimpleKeyframe(shape1, 0);
+    Keyframe kf2 = new SimpleKeyframe(shape2, 40);
+    assertEquals(kf1.interpolate(kf2, 10), interpolatedShape);
   }
 }
