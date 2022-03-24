d4afcc3be195c43fa451d6a0375802ae21f3ba53
Joel Willick <willickjb@gmail.com>
2022-03-23 22:27:16 -0400

    implementation SimpleKeyframe.Interpolate()

--- src/cs3500/animator/model/SimpleKeyframe.java
+++ src/cs3500/animator/model/SimpleKeyframe.java
@@ -1,20 +1,23 @@
 package cs3500.animator.model;
 
+import java.awt.*;
+
 /**
  * Represents a shape at a time in an animation.
  */
-public class SimpleKeyframe implements Keyframe{
+public class SimpleKeyframe implements Keyframe {
 
   private final Shape shape;
   private final int time;
 
   /**
    * Constructor for SimpleKeyframe.
+   *
    * @param shape the shape of this keyframe
-   * @param time the time that this keyframe exists at
+   * @param time  the time that this keyframe exists at
    */
   public SimpleKeyframe(Shape shape, int time) {
-    if(shape == null) {
+    if (shape == null) {
       throw new IllegalArgumentException("Shape cannot be null");
     }
     this.shape = shape;
@@ -52,10 +55,35 @@ public class SimpleKeyframe implements Keyframe{
    */
   @Override
   public Shape interpolate(Keyframe otherKeyframe, int interpolateTime) {
-    return null;
+    if (otherKeyframe == null) {
+      throw new IllegalArgumentException("otherKeyframe must not be null");
+    }
+    if (interpolateTime < otherKeyframe.getTime() && interpolateTime < time ||
+            interpolateTime > otherKeyframe.getTime() && interpolateTime > time) {
+      throw new IllegalArgumentException("interpolateTime must be between the two times of the keyframes");
+    }
+    if (!(shape.getName().equals(otherKeyframe.getShape().getName()))) {
+      throw new IllegalArgumentException("The shapes of both keyframes must have the same names");
+    }
+    if (shape.getType() != otherKeyframe.getShape().getType()) {
+      throw new IllegalArgumentException("The shapes of both keyframes must be of the same type");
+    }
+    float x = interpolate(time, interpolateTime, otherKeyframe.getTime(), shape.getX(), otherKeyframe.getShape().getX());
+    float y = interpolate(time, interpolateTime, otherKeyframe.getTime(), shape.getY(), otherKeyframe.getShape().getY());
+    float height = interpolate(time, interpolateTime, otherKeyframe.getTime(), shape.getHeight(), otherKeyframe.getShape().getHeight());
+    float width = interpolate(time, interpolateTime, otherKeyframe.getTime(), shape.getWidth(), otherKeyframe.getShape().getWidth());
+    int red = (int) interpolate(time, interpolateTime, otherKeyframe.getTime(), shape.getColor().getRed(), otherKeyframe.getShape().getColor().getRed());
+    int green = (int) interpolate(time, interpolateTime, otherKeyframe.getTime(), shape.getColor().getGreen(), otherKeyframe.getShape().getColor().getGreen());
+    int blue = (int) interpolate(time, interpolateTime, otherKeyframe.getTime(), shape.getColor().getBlue(), otherKeyframe.getShape().getColor().getBlue());
+
+    return SimpleShapeFactory.getShape(shape.getType(), shape.getName(), x, y, new Color(red, green, blue), height, width);
   }
-  
-  
+
+  private float interpolate(float t0, float t1, float t2, float value1, float value2) {
+    return (value2 - value1) * (1 - ((t2 - t1) / (t2 - t0))) + value1;
+  }
+
+
   @Override
   public String getDescription(Keyframe other) {
     Shape shapeEnd = other.getShape();
@@ -68,7 +96,7 @@ public class SimpleKeyframe implements Keyframe{
       move = s + "stays put at (" + this.shape.getX() + "," + this.shape.getY() + "), ";
     } else {
       move = s + "moves from (" + this.shape.getX() + "," + this.shape.getY() + ") to ("
-      + shapeEnd.getX() + "," + shapeEnd.getY() + "), ";
+              + shapeEnd.getX() + "," + shapeEnd.getY() + "), ";
     }
     if (this.shape.getWidth() == shapeEnd.getWidth()
             && this.shape.getHeight() == shapeEnd.getHeight()) {
@@ -88,7 +116,7 @@ public class SimpleKeyframe implements Keyframe{
 
   @Override
   public boolean equals(Object o) {
-    if(o instanceof Keyframe) {
+    if (o instanceof Keyframe) {
       Keyframe other = (Keyframe) o;
       return this.shape.equals(other.getShape()) &&
               this.time == other.getTime();
