9b25878b48d7c7d2b53066953668d07f64c1a76f
Joel Willick <willickjb@gmail.com>
2022-04-03 17:28:48 -0400

    Main saves to the correct file extension

--- src/cs3500/animator/Main.java
+++ src/cs3500/animator/Main.java
@@ -22,20 +22,20 @@ public class Main {
     String out = "System.out";
     String viewType = null;
     int speed = 1;
-    for(int i = 0; i < args.length - 1; i++) {
+    for (int i = 0; i < args.length - 1; i++) {
       switch (args[i]) {
         case "-in":
-          in = args[i+1];
+          in = args[i + 1];
           break;
         case "-out":
-          out = args[i+1];
+          out = args[i + 1];
           break;
         case "-view":
-          viewType = args[i+1];
+          viewType = args[i + 1];
           break;
         case "-speed":
           try {
-            speed = Integer.parseInt(args[i+1]);
+            speed = Integer.parseInt(args[i + 1]);
           } catch (NumberFormatException nfe) {
             Frame frame = new Frame();
             JOptionPane.showMessageDialog(frame, "Error: -speed must be an int");
@@ -44,11 +44,11 @@ public class Main {
           break;
       }
     }
-    if(in == null) {
+    if (in == null) {
       Frame frame = new Frame();
       JOptionPane.showMessageDialog(frame, "Error: must specify input text file");
       throw new IllegalArgumentException("you must specify -in");
-    } else if(viewType == null) {
+    } else if (viewType == null) {
       Frame frame = new Frame();
       JOptionPane.showMessageDialog(frame, "Error: must specify input text file");
       throw new IllegalArgumentException("you must specify -view");
@@ -78,11 +78,15 @@ public class Main {
     }
 
     // output the view to the appropriate location
-    if(out.equals("System.out")) {
+    if (out.equals("System.out")) {
       System.out.println(appendable);
     } else {
+      String extension = ".txt";
+      if(viewType.equals("svg")) {
+        extension = ".svg";
+      }
       try {
-        Files.writeString(Paths.get(out), appendable.toString());
+        Files.writeString(Paths.get(out + extension), appendable.toString());
       } catch (IOException e) {
         Frame frame = new Frame();
         JOptionPane.showMessageDialog(frame, "Error: was unable to save to -out");
